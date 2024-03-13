ab12712963249ebeb8ffd52cad8381480f4b16f1
= <=>
2024-03-13 21:49:57 +0530

    with @BeforeEach

--- JunitMavenProject/src/test/java/com/rv/ContactManagerTest.java
+++ JunitMavenProject/src/test/java/com/rv/ContactManagerTest.java
@@ -1,15 +1,22 @@
 package com.rv;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ContactManagerTest {
 
+        private ContactManager contactManager;
+        @BeforeEach
+        void setUp(){
+                System.out.println("Inside before each");
+                contactManager = new ContactManager();
+        }
+
         @Test
         @DisplayName("Test case for creating contact successfully.........")
         public void shouldCreateContact(){
-                ContactManager contactManager = new ContactManager();
                 contactManager.addContact("Ravi","Prakash","0123456789");
                 Assertions.assertEquals(1,contactManager.getAllContact().size());
                 Assertions.assertFalse(contactManager.getAllContact().isEmpty());
@@ -18,7 +25,6 @@ public class ContactManagerTest {
         @Test
         @DisplayName("Test case for Not creating contact with first name.......")
         public void validateFirstNameEmpty(){
-                ContactManager contactManager = new ContactManager();
                 Assertions.assertThrows(RuntimeException.class,()->{
                    contactManager.addContact(null,"Prakash","0123456789");
                 });
@@ -27,7 +33,6 @@ public class ContactManagerTest {
         @Test
         @DisplayName("Test case for not creating contact with the last name....")
         public void validateLastNameEmpty(){
-                ContactManager contactManager = new ContactManager();
                 Assertions.assertThrows(RuntimeException.class,
                         ()->contactManager.addContact("Ravi",null,"0123456789"));
         }
@@ -35,8 +40,30 @@ public class ContactManagerTest {
         @Test
         @DisplayName("Test case for not creating contact with contact number....")
         public void validateContactNumber(){
-                ContactManager contactManager = new ContactManager();
                 Assertions.assertThrows(RuntimeException.class,
                         ()->contactManager.addContact("Ravi","Prakash",null));
+        }
+
+        @Test
+        @DisplayName("Test case for contact size not equals to 10..")
+        public void validateContactNumberSize(){
+                Assertions.assertThrows(RuntimeException.class,
+                        ()->contactManager.addContact("Ravi","Prakash","01234567891"));
+        }
+
+        @Test
+        @DisplayName("Test case for contact not starting with zero..")
+        public void validateContactNotStartingWithZero(){
+                Assertions.assertThrows(RuntimeException.class,
+                        ()->contactManager.addContact("Ravi","Prakash","1234567890"));
+        }
+
+        @Test
+        @DisplayName("Test for existing contact..->")
+        public void validateContactAlreadyExist(){
+                contactManager.addContact("Ravi","Prakash","0123456789");
+                Assertions.assertThrows(RuntimeException.class,
+                        ()-> contactManager.addContact("Ravi","Prakash","0123456789"),
+                        "Contact already exist");
         }
 }
